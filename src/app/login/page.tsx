3f3ccab77d036f06ed5f84ed029f6f3b2cd0393e
Aytaditya <adityaaryan531@gmail.com>
2024-06-08 03:07:25 +0530

    login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,60 @@
+"use client"
+
+import {useState} from "react"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import  axios  from "axios"
+import Image from "next/image"
+
+
 export default function LoginPage() {
-    return(
-        <div>
-        login
+    const [user,setUser] = useState({
+        email: "",
+        password: "",
+    })
+    const onSignup=async(e:any)=>{
+        e.preventDefault();
+        console.log(user)
+    }
+    const handleDisable =():boolean=>{
+        if(user.email=="" || user.password==""){
+            return true
+    }
+    return false
+}
+
+    return (
+        <div className="flex flex-row bg-custom h-screen ">
+            <div className="flex flex-col  px-4 py-6 min-h-[300px] w-[35%] upper-layer m-10">
+            <h1 className="text-3xl flex items-center justify-center font-bold">Login to &nbsp; <span className="text-[#ad36ad] font-bold text-4xl">App</span></h1>
+            <p className="flex items-center justify-center flex-col mt-3 text-gray-400">Login to Unlock all the features of app</p>
+            <form action="" className="mt-10" onSubmit={onSignup}>
+                {/* first input */}
+            <div className="flex flex-col mt-5">
+            <label htmlFor="email" className="text-white">Email</label>
+            <input type="email" placeholder="aditya@gmail.com" className="w-[70%] bg-transparent border-b-[1px] mt-1 rounded-sm px-2 font-light text-white" value={user.email} onChange={(e)=>setUser({...user,email:e.target.value})} />
+            </div>
+            
+            {/* second input */}
+            <div className="flex flex-col mt-5">
+            <label htmlFor="password" className="text-white">Password</label>
+            <input type="password" className="w-[70%] bg-transparent border-b-[1px] mt-1 rounded-sm px-2 font-light text-white" value={user.password} onChange={(e)=>setUser({...user,password:e.target.value})} />
+            </div>
+            <p className="text-sm mt-5 text-gray-400 mx-3">Dont have an Account? 
+             <Link href="/signup" className="mx-2 text-[#ef48ef] font-bold hover:underline">Signup</Link>
+
+            </p>
+            {/* submit button */}
+            <div className="flex flex-col items-center mt-[50px]">
+            <button className="bg-[purple] w-[40%] text-white px-4 py-2 rounded-lg font-semibold mr-2 " disabled={handleDisable()}>Login</button>
+            </div>
+            </form>
+        </div>
+        <div className="m-[100px]">
+        <Image src="/hello.svg" alt="logo" width={650} height={250} className="my-[50px]" loading="lazy" layout="intrinsic" />
+        </div>
+        
+            
         </div>
     )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,8 +3,9 @@
 import {useState} from "react"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
-import { axios } from "axios"
+import  axios  from "axios"
 import Image from "next/image"
+
 
 export default function SignupPage() {
     const [user,setUser] = useState({
@@ -12,28 +13,42 @@ export default function SignupPage() {
         password: "",
         username: ""
     })
-    const onSignup=async()=>{
-
+    const onSignup=async(e:any)=>{
+        e.preventDefault();
+        console.log(user)
     }
     return (
         <div className="flex flex-row bg-custom h-screen ">
             <div className="flex flex-col  px-4 py-6 min-h-[500px] w-[35%] upper-layer m-10">
-            <h1 className="text-3xl flex items-center justify-center font-bold">SignUp to &nbsp; <span className="text-[purple] font-bold text-4xl">App</span></h1>
-            <form action="" className="mt-5">
+            <h1 className="text-3xl flex items-center justify-center font-bold">SignUp to &nbsp; <span className="text-[#ad36ad] font-bold text-4xl">App</span></h1>
+            <form action="" className="mt-5" onSubmit={onSignup}>
                 {/* first input */}
-            <div className="flex flex-col mt-3">
+            <div className="flex flex-col mt-5">
             <label htmlFor="username" className="text-white">Username</label>
-            <input type="text" placeholder="aytaditya" className="w-[60%] bg-transparent border-b-[1px] mt-1 rounded-sm px-2 font-light text-white" value={user.username} onChange={(e)=>setUser({...user,username:e.target.value})} />
+            <input type="text" placeholder="aytaditya" className="w-[70%] bg-transparent border-b-[1px] mt-1 rounded-sm px-2 font-light text-white" value={user.username} onChange={(e)=>setUser({...user,username:e.target.value})} />
             </div>
             {/* second input */}
-            <div className="flex flex-col mt-3">
+            <div className="flex flex-col mt-5">
             <label htmlFor="email" className="text-white">Email</label>
-            <input type="email" placeholder="aditya@gmail.com" className="w-[60%] bg-transparent border-b-[1px] mt-1 rounded-sm px-2 font-light text-white" value={user.email} onChange={(e)=>setUser({...user,username:e.target.value})} />
+            <input type="email" placeholder="aditya@gmail.com" className="w-[70%] bg-transparent border-b-[1px] mt-1 rounded-sm px-2 font-light text-white" value={user.email} onChange={(e)=>setUser({...user,email:e.target.value})} />
+            </div>
+            {/* third input */}
+            <div className="flex flex-col mt-5">
+            <label htmlFor="password" className="text-white">Password</label>
+            <input type="password" className="w-[70%] bg-transparent border-b-[1px] mt-1 rounded-sm px-2 font-light text-white" value={user.password} onChange={(e)=>setUser({...user,password:e.target.value})} />
+            </div>
+            <p className="text-sm mt-5 text-gray-400 mx-3">Already have an Account? 
+             <Link href="/login" className="mx-2 text-[#ef48ef] font-bold hover:underline">Login</Link>
+
+            </p>
+            {/* submit button */}
+            <div className="flex flex-col items-center mt-[120px]">
+            <button className="bg-[purple] w-[40%] text-white px-4 py-2 rounded-lg font-semibold mr-2 hover:bg-[#501b50]">SignUp</button>
             </div>
             </form>
         </div>
         <div className="m-[100px]">
-        <Image src="/hello.svg" alt="logo" width={560} height={150} className="my-[50px]" loading="lazy" layout="intrinsic" />
+        <Image src="/hello.svg" alt="logo" width={650} height={250} className="my-[50px]" loading="lazy" layout="intrinsic" />
         </div>
         
             
