afbe4b0ecc3b9429969a7cafc0f3af3065816557
Aytaditya <adityaaryan531@gmail.com>
2024-06-16 21:32:03 +0530

    logout backend & frontend implemented

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,23 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import axios from "axios"
+
 export default function ProfilePage() {
+    const router=useRouter()
+    const logout=async()=>{
+        try {
+            const response= await axios.get("/api/users/logout")
+            router.push("/login")
+            
+        } catch (error:any) {
+            console.log(error.message)
+        }
+    }
     return (
-        <div>
-            Profile Page
+        <div className="p-5">
+            <h1 className="text-[purple] text-3xl mb-4">Pofile page</h1>
+            <button className="bg-[purple] w-[20%] text-white px-4 py-2 rounded-lg font-semibold mr-2 hover:bg-[#501b50]" onClick={logout} >Logout</button>
         </div>
     )
 }
